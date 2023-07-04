3d1e5ef10f1cb5c54701a9a783436e363efcbbad
StockerMC <44980366+StockerMC@users.noreply.github.com>
2023-07-04 17:37:17 -0400

    Remove ChallengeData.appElement and reorder challenges

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,14 +1,8 @@
-
-// import { default as QRCodeApp} from './challenges/qr-code-component/App';
-// import { default as AgeCalculatorApp} from './challenges/age-calculator/App';
-import { ReactElement } from 'react';
-
 class ChallengeData {
     constructor(
         readonly path: string,
         readonly title: string,
         readonly description: string,
-        readonly appElement: ReactElement
     ) {}
 }
 
@@ -17,30 +11,25 @@ export const cards: ChallengeData[] = [
         "qr-code-component",
         "QR Code Component",
         "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        <p>TEST</p>,
     ),
     new ChallengeData(
         "interactive-rating-component",
         "Interactive Rating Component",
         "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        <p>TEST</p>,
-    ),
-    new ChallengeData(
-        "age-calculator-app",
-        "Age Calculator App",
-        "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        <p>TEST</p>,
-    ),
-    new ChallengeData(
-        "calculator-app",
-        "Calculator App",
-        "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        <p>TEST</p>,
     ),
     new ChallengeData(
         "rock-paper-scissors",
         "Rock, Paper, Scissors",
         "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        <p>TEST</p>,
+    ),
+    new ChallengeData(
+        "age-calculator-app",
+        "Age Calculator App",
+        "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
+    ),
+    new ChallengeData(
+        "calculator-app",
+        "Calculator App",
+        "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
     ),
 ];
